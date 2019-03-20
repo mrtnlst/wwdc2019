e20f4b6556661f83d282c6d84199fc0cebbdae48
Martin List <martinlist@me.com>
2019-03-20 18:02:05 +0100

    Adds MediaPlayer request

--- wwdc2019.playground/Contents.swift
+++ wwdc2019.playground/Contents.swift
@@ -1,14 +1,16 @@
 import PlaygroundSupport
 import UIKit
+import MediaPlayer
 
-// Gathering test data.
-var albums = [Album]()
-for index in 1...10 {
-    if let artwork = UIImage(named: "artwork\(index).jpg") {
+var sourceAlbums = [Album]()
+if let albums = MPMediaQuery.albums().collections {
+    for album in albums {
+        guard let artwork = (album.representativeItem?.value(forProperty: MPMediaItemPropertyArtwork) as? MPMediaItemArtwork)?.image(at: CGSize(width: 60, height: 60)) else { continue }
+        
         let artworkColors = artwork.getColors()
         var colorsForAlbum = [Color]()
         if let primaryColor = artworkColors.primary.closestBasicColor() {
-            colorsForAlbum.append(primaryColor)
+            colorsForAlbum.append(primaryColor)            
         }
         if let secondaryColor = artworkColors.secondary.closestBasicColor() {
             colorsForAlbum.append(secondaryColor)
@@ -19,10 +21,35 @@ for index in 1...10 {
         if let detailColor = artworkColors.detail.closestBasicColor() {
             colorsForAlbum.append(detailColor)
         }
-        albums.append(Album(artwork: artwork, mediaID: "\(index)", colors: colorsForAlbum))
+        guard let mediaID = (album.representativeItem?.value(forProperty: MPMediaItemPropertyAlbumPersistentID) as? NSNumber)?.stringValue else { continue }
+        
+        guard let title = album.representativeItem?.value(forProperty: MPMediaItemPropertyAlbumTitle) as? String else { continue }
+        
+        sourceAlbums.append(Album(artwork: artwork, mediaID: mediaID, colors: colorsForAlbum))
     }
 }
+// Gathering test data.
+
+//  for index in 1...10 {
+//      if let artwork = UIImage(named: "artwork\(index).jpg") {
+//          let artworkColors = artwork.getColors()
+//          var colorsForAlbum = [Color]()
+//          if let primaryColor = artworkColors.primary.closestBasicColor() {
+//              colorsForAlbum.append(primaryColor)
+//          }
+//          if let secondaryColor = artworkColors.secondary.closestBasicColor() {
+//              colorsForAlbum.append(secondaryColor)
+//          }
+//          if let backgroundColor = artworkColors.background.closestBasicColor() {
+//              colorsForAlbum.append(backgroundColor)
+//          }
+//          if let detailColor = artworkColors.detail.closestBasicColor() {
+//              colorsForAlbum.append(detailColor)
+//          }
+//          albums.append(Album(artwork: artwork, mediaID: "\(index)", colors: colorsForAlbum))
+//      }
+//  }
 
 // Initialising view controller.
-let viewController = ViewController(sourceAlbums: albums)
+let viewController = ViewController(sourceAlbums: sourceAlbums)
 PlaygroundPage.current.liveView = viewController
