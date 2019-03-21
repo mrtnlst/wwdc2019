c52b8a80a2a7bdf98fa6fb48c8ccef74727d43c6
Martin List <martinlist@me.com>
2019-03-21 14:04:20 +0100

    - Adds commentary

--- wwdc2019.playground/Contents.swift
+++ wwdc2019.playground/Contents.swift
@@ -1,4 +1,7 @@
 // Created by Martin List as a WWDC19 Scholarship submission.
+//
+// To experience this playground, please make sure to have at
+// least 5-30 albums in the iPad's music app. 
 
 import PlaygroundSupport
 import UIKit
