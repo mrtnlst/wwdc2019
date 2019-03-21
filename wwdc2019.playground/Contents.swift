aba4b7ed084d93e1e1c5085f9867410e36df5b13
Martin List <martinlist@me.com>
2019-03-21 12:39:54 +0100

    - Adds license of 3rd party code - Adds comment to Contents

--- wwdc2019.playground/Contents.swift
+++ wwdc2019.playground/Contents.swift
@@ -1,6 +1,9 @@
 import PlaygroundSupport
 import UIKit
 
+// The RootViewController provides an introduction to the app and
+// asks for access to your local music library.
+
 let rootViewController = RootViewController()
 PlaygroundPage.current.liveView = rootViewController
 PlaygroundPage.current.needsIndefiniteExecution = true
